ef582f2f3cb4774ca9c2c9df685cbfdb50bbcafb
Edhendil <edhendil@gmail.com>
2013-10-03 22:03:51 +0200

    Refactored logical query
    It's cleaner now but I still feel it can be done better

--- MonoLiteOrm/LogicalQuery/LogicalQuery.cs
+++ MonoLiteOrm/LogicalQuery/LogicalQuery.cs
@@ -7,28 +7,32 @@ namespace Mono.Mlo
 	/// Logical query.
 	/// All tables are added to the join tree. If a pair of logical tables is a part of the same physical table then no join is performed.
 	/// Also the returned queryTable object will have the same alias for both logical tables.
-	/// 
-	/// Does not support self join (every employee has a manager). With join table it will work.
 	/// </summary>
 	public class LogicalQuery
 	{
 		
-		private List<LogicalQueryTable> logicalTables = new List<LogicalQueryTable>();
-		private List<PhysicalQueryTable> physicalTables = new List<PhysicalQueryTable>();
+		// to know what columns should be used as a link between tables
+		private List<LogicalQueryTable> logicalTables;
+		// to know what columns should be selected
+		private List<PhysicalQueryTable> physicalTables;
 		
+		// small addition to ease developers life (altough it complicates a bit code in this class)
 		private LogicalQueryTable lastTableAdded;
 		
+		// refreshed after every join
 		private ITableExpression fromSource;
 		
 		public LogicalQuery ()
 		{
+			this.logicalTables = new List<LogicalQueryTable>();
+			this.physicalTables = new List<PhysicalQueryTable>();
 		}
 		
 		// to be used for consecutive joins without forks
 		public virtual LogicalQueryTable JoinTable(LogicalTable table) {
 			
 			if (this.lastTableAdded == null) {
-				setRootTable(table);
+				setRootTable (table);
 			} else {
 				attachTable (lastTableAdded, table);
 			}
@@ -49,6 +53,7 @@ namespace Mono.Mlo
 		public virtual Query ToNativeQuery() {
 			Query result = new Query();
 			
+			// attach previously created source
 			result.From.Source = this.fromSource;
 			
 			// add columns
@@ -60,55 +65,73 @@ namespace Mono.Mlo
 		}
 		
 		private void setRootTable(LogicalTable rootTable) {
-			
-			string alias = createTableAlias(rootTable.PartOfTable.Name, 0);
+			string alias = createTableAlias(rootTable);
 			LogicalQueryTable queryTable = new LogicalQueryTable() {Table = rootTable, Alias = alias};
 			PhysicalQueryTable physicalQueryTable = new PhysicalQueryTable() {Table = rootTable.PartOfTable, Alias = alias};
 			this.logicalTables.Add (queryTable);
 			this.physicalTables.Add (physicalQueryTable);
 			this.fromSource = physicalQueryTable.ToTableReference();
 			this.lastTableAdded = queryTable;
-			
 		}
 		
 		private void attachTable(LogicalQueryTable source, LogicalTable joinWith) {
 			
-			// check if join can be performed
+			// check if join can be performed, if not then exception is thrown
+			validateAttachArguments(source, joinWith);
+			
+			// join tables
+			LogicalQueryTable target = new LogicalQueryTable() {Table = joinWith};
+			
+			// if the join table is in fact a part of another physical table already included in source logical table
+			// then dont join them
+			// always join tables if the target table is of entity type
+			if (!isJoinRequired (source, target)) {
+				target.Alias = source.Alias;
+			} else {
+				string alias = createTableAlias(joinWith);
+				target.Alias = alias;
+				
+				PhysicalQueryTable physicalQueryTable = new PhysicalQueryTable() {Table = joinWith.PartOfTable, Alias = alias};
+				this.physicalTables.Add (physicalQueryTable);
+				
+				this.fromSource = createNewJoin (this.fromSource, source, target);
+			}
+			
+			this.logicalTables.Add (target);
+			this.lastTableAdded = target;
+			
+		}
+		
+		private bool isJoinRequired(LogicalQueryTable source, LogicalQueryTable target) {
+			return source.IsLogicalTableEqual(target) && target.Table.TableType == LogicalTableType.JOIN;
+		}
+		
+		private void validateAttachArguments(LogicalQueryTable source, LogicalTable joinWith) {
 			if (source.Table.Equals(joinWith)) {
 				throw new ArgumentException("Cannot join a logical table with itself");
 			} else if (source.Table.TableType.Equals (joinWith.TableType)) {
 				throw new ArgumentException("Cannot join two entity logical or two join logical tables");
 			}
-			
-			// join tables
-			LogicalQueryTable logicalTable = new LogicalQueryTable() {Table = joinWith};
-			if (source.Table.IsPartOfTheSameTable(joinWith)) {
-				logicalTable.Alias = source.Alias;
-			} else {
-				string alias = createTableAlias(joinWith.PartOfTable.Name, howManyTimesUsed(joinWith));
-				logicalTable.Alias = alias;
-				
-				PhysicalQueryTable physicalQueryTable = new PhysicalQueryTable() {Table = joinWith.PartOfTable, Alias = alias};
-				this.physicalTables.Add (physicalQueryTable);
-				
-				LogicalQueryTable relationTable = (source.Table.TableType == LogicalTableType.JOIN ? source : logicalTable);
-				LogicalQueryTable entityTable = (source.Table.TableType == LogicalTableType.ENTITY ? source : logicalTable);
-				
-				TableColumn primaryKey = entityTable.Table.GetPrimaryKey();
-				TableColumn foreignKey = relationTable.Table.GetForeignKey(primaryKey);
-				
-				ILogicalCondition on = Logical.Equal(Expression.Column(entityTable.Alias, primaryKey.Name), Expression.Column (relationTable.Alias, foreignKey.Name));
-				
-				this.fromSource = From.Left (this.fromSource, logicalTable.ToTableReference(), on);
-			}
-			
-			this.logicalTables.Add (logicalTable);
-			this.lastTableAdded = logicalTable;
-			
 		}
 		
-		private int howManyTimesUsed(LogicalTable table) {
-			return this.physicalTables.FindAll ((x) => x.Table.Equals(table.PartOfTable)).Count;
+		private ITableExpression createNewJoin(ITableExpression oldJoin, LogicalQueryTable sourceTable, LogicalQueryTable targetTable) {
+			LogicalQueryTable relationTable = (sourceTable.Table.TableType == LogicalTableType.JOIN ? sourceTable : targetTable);
+			LogicalQueryTable entityTable = (sourceTable.Table.TableType == LogicalTableType.ENTITY ? sourceTable : targetTable);
+				
+			TableColumn primaryKey = entityTable.Table.GetPrimaryKey();
+			TableColumn foreignKey = relationTable.Table.GetForeignKey(primaryKey);
+				
+			ILogicalCondition on = Logical.Equal(Expression.Column(entityTable.Alias, primaryKey.Name), Expression.Column (relationTable.Alias, foreignKey.Name));
+				
+			return From.Left (oldJoin, targetTable.ToTableReference(), on);
+		}
+		
+		private int howManyTimesUsed(TableDefinition table) {
+			return this.physicalTables.FindAll ((x) => x.Table.Equals(table)).Count;
+		}
+		
+		private string createTableAlias(LogicalTable logicalTable) {
+			return createTableAlias(logicalTable.PartOfTable.Name, howManyTimesUsed(logicalTable.PartOfTable));
 		}
 		
 		private string createTableAlias(string tableName, int index) {
